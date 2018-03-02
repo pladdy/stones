aaea234a197656ea0f1957b723a0c954c50e8d77
Matt Pladna <pladdypants@gmail.com>
2018-03-01 22:59:27 -0500

    update make; fix json tags

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+all: dependencies test
+
 cover:
 	go test -v -coverprofile=cover.out
 	go tool cover -func=cover.out
--- bundle.go
+++ bundle.go
@@ -2,10 +2,10 @@ package stones
 
 // Bundle is a STIX bundle: a collection of STIX objects
 type Bundle struct {
-	Type        string   `json:type`
-	ID          StixID   `json:id`
-	SpecVersion string   `json:spec_version`
-	Objects     []string `json:objects`
+	Type        string   `json:"type"`
+	ID          StixID   `json:"id"`
+	SpecVersion string   `json:"spec_version"`
+	Objects     []string `json:"objects"`
 }
 
 // NewBundle returns a STIX bundle object
