16a371cdb46f8e3106206fa2c0558ec7355467df
Matt Pladna <pladdypants@gmail.com>
2018-11-20 16:12:20 -0500

    add validation; add tags

--- objects.go
+++ objects.go
@@ -1,28 +1,48 @@
 package stones
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 // Object is a generic STIX object with properties common to all SIIX objects
 type Object struct {
-	// required
-	ID       Identifier `json:"id"`
-	Type     string     `json:"type"`
-	Created  Timestamp  `json:"created"`
-	Modified Timestamp  `json:"modified"`
-	// optional
-	CreatedByRef       Identifier          `json:"created_by_ref,omitempty"`
-	Revoked            bool                `json:"revoked,omitempty"`
-	Labels             []string            `json:"labels,omitempty"`
-	ExternalReferences []ExternalReference `json:"external_references,omitempty"`
-	ObjectMarkingRefs  []Identifier        `json:"object_marking_refs,omitempty"`
-	GranularMarkings   []string            `json:"granular_markings,omitempty"`
-	// track the original json
-	Source []byte
+	ID                 Identifier          `json:"id" stones:"required"`
+	Type               string              `json:"type" stones:"required"`
+	Created            Timestamp           `json:"created" stones:"required"`
+	Modified           Timestamp           `json:"modified" stones:"required"`
+	CreatedByRef       Identifier          `json:"created_by_ref,omitempty" stones:"optional"`
+	Revoked            bool                `json:"revoked,omitempty" stones:"optional"`
+	Labels             []string            `json:"labels,omitempty" stones:"optional"`
+	ExternalReferences []ExternalReference `json:"external_references,omitempty" stones:"optional"`
+	ObjectMarkingRefs  []Identifier        `json:"object_marking_refs,omitempty" stones:"optional"`
+	GranularMarkings   []string            `json:"granular_markings,omitempty" stones:"optional"`
+	Source             []byte              `stones:"optional"`
 }
 
 // NewObject returns an object based on JSON input in bytes
 func NewObject(b []byte) (o Object, err error) {
 	err = json.Unmarshal(b, &o)
 	o.Source = b
+	return
+}
+
+// Valid returns whether the objecti s valid or not
+func (o *Object) Valid() (valid bool, errs []error) {
+	if !validStixType(o.Type) {
+		errs = append(errs, fmt.Errorf("Invalid STIX type: %v", o.Type))
+	}
+
+	if valid, err := o.ID.Valid(); !valid {
+		errs = append(errs, fmt.Errorf("Invalid ID: %v, Error: %v", o.ID, err))
+	}
+
+	if o.Type != o.ID.Type {
+		errs = append(errs, fmt.Errorf("Object 'Type' (%v) and 'Identfier' Type (%v) don't match", o.Type, o.ID.Type))
+	}
+
+	if len(errs) == 0 {
+		valid = true
+	}
 	return
 }
--- objects_test.go
+++ objects_test.go
@@ -32,3 +32,35 @@ func TestObjectSerializeDeserialize(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestObjectValid(t *testing.T) {
+	tests := []struct {
+		testFile    string
+		expectError bool
+	}{
+		{"testdata/malware.json", false},
+		{"testdata/malware_invalid_type.json", true},
+		{"testdata/malware_type_mismatch.json", true},
+	}
+
+	for _, test := range tests {
+		b, err := ioutil.ReadFile(test.testFile)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		o, err := NewObject(b)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, errs := o.Valid()
+		if test.expectError && len(errs) == 0 {
+			t.Error("Expected error, Test:", test, "Object:", o)
+		}
+
+		if !test.expectError && len(errs) > 0 {
+			t.Error("Error unexpected, Test:", test, "Object:", o, "Errors:", errs)
+		}
+	}
+}
