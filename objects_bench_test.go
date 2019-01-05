fb8da0d26a63f32b8f58c672af7b597f9b87ef24
Matt Pladna <pladdypants@gmail.com>
2019-01-05 13:09:09 -0500

    update benchmarks

--- objects_bench_test.go
+++ objects_bench_test.go
@@ -1,12 +1,33 @@
 package stones
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"testing"
 )
 
-// not sure if this benchmark is useful; it's a small json file/object...
-func BenchmarkNewObject(b *testing.B) {
+func BenchNewObject(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		NewObject("malware")
+		NewObject("bundle")
+	}
+}
+
+func BenchObjectValid(b *testing.B) {
+	o, _ := NewObject("test")
+
+	for i := 0; i < b.N; i++ {
+		o.Valid()
+	}
+}
+
+func BenchUnmarshalObject(b *testing.B) {
+	d, err := ioutil.ReadFile("testdata/malware.json")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		var o Object
+		json.Unmarshal(d, &o)
 	}
 }
--- timestamp_bench_test.go
+++ timestamp_bench_test.go
@@ -1,11 +1,23 @@
 package stones
 
 import (
+	"encoding/json"
 	"testing"
 	"time"
 )
 
 var now = time.Now().UTC()
+
+func BenchmarkMarshalTimestamp(b *testing.B) {
+	ts, err := NewTimestamp("2016-04-06T20:07:09.000Z")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		json.Marshal(ts)
+	}
+}
 
 func BenchmarkNewTimestamp(b *testing.B) {
 	for i := 0; i < b.N; i++ {
